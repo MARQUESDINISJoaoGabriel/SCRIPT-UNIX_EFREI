59135a2467af1764b1f5349368eccf8eebd8f6bb
Dark01213 <Cedrichu01@gmail.com>
2024-12-10 15:47:21 +0100

    ajout de scan.sh

--- scan.sh
+++ scan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Partie 1
+#sudo ufw disable pour éteindre le pare-feu de l'ordinateur ;
 echo "Choississez un scan parmis les suivant :
         Scan simple : 1
         scan complet : 2
@@ -10,15 +11,15 @@ read reponse
 if [ "$reponse" -eq '1' ]; then
         echo " donnez l'ip à rechercher ":
         read ip_1
-        sudo nmap -pn $ip_1 ;
+        sudo nmap -Pn $ip_1 ;
 fi
 if [ "$reponse" -eq '2' ]; then
-        sudo nmap -pn -p- ;
+        sudo nmap -Pn -p- ;
 fi
 if [ "$reponse" -eq '3' ]; then
         echo " veuilez donnez l'ip à analyser " ;
         read ip ;
-        sudo nmap -p "$ip" ;
+        sudo nmap -P "$ip" ;
 fi
 #if [ "$reponse" -eq '4' ]; then
 #       read -p "Entrez les adresses IP (séparées par des virgules) : " ips
