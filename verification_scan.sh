cd6b2cb6484fd84ff431d3b0bc4d877d54c840de
Dark01213 <Cedrichu01@gmail.com>
2024-12-10 15:32:39 +0100

    modification de verification_scan

--- verification_scan.sh
+++ verification_scan.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 echo "Choississez un scan parmis les suivant :
-	Scan simple : 1 ,
-	scan complet : 2 ,
-	scan personnalisé(singulier) : 3 . 
+	Scan simple : 1 
+	scan complet : 2 
+	scan personnalisé(singulier) : 3  
 	scan personalisé(plusieurs) : 4 " ;
 read reponse
 
 if [ "$reponse" -eq '1' ]; then
-	ip_all=nmap -Pn -A ;
-	nmap --port-ratio "$ip_all" ;
-	nmap -sp "$ip_all" ; 
+        echo " donnez l'ip à rechercher ":
+        read ip_1
+        nmap -pn "$ip_1" ; 
 fi	
-if [ "$reponse" -eq '2' ]; then 
-	nmap -p- ; 
-fi
+if [ "$reponse" -eq '2' ]; then 	
+	sudo nmap -p 0-65335 ; 
+fi 
 if [ "$reponse" -eq '3' ]; then
 	echo " veuilez donnez l'ip à analyser " ;
 	read ip ;
-	nmap -p "$ip" ;
+	sudo nmap -p "$ip" ;
 fi
 #if [ "$reponse" -eq '4' ]; then
 #	read -p "Entrez les adresses IP (séparées par des virgules) : " ips
